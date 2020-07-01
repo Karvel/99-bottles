56b75c78141b2dc19d15d50078f4cefc9f25187f
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-30 21:45:17 -0700

    chore, test(test): add test dependencies

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require_relative '../lib/bottles'
